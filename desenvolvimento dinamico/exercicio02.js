8f512adcd71efa584d39fc5b1255497f4e20d20c
igorfariasoliveira <igor10farias@gmail.com>
2024-02-05 14:40:56 -0300

    Update exercicio02.js

--- desenvolvimento dinamico/exercicio02.js
+++ desenvolvimento dinamico/exercicio02.js
@@ -1,1 +1,5 @@
-let meses = ["Janeiro","Fevereiro","Janeiro","Janeiro","Janeiro","Janeiro","Janeiro","Janeiro","Janeiro","Janeiro","Janeiro","Janeiro",]
+let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro",];
+
+let primeiroTrimestre = meses.slice(0,3)
+
+console.log(primeiroTrimestre)
