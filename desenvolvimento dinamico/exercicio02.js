de70c42d16dbf6591eb2faaa9ed79fe1182b8431
igorfariasoliveira <igor10farias@gmail.com>
2024-02-05 14:47:02 -0300

    Update exercicio02.js

--- desenvolvimento dinamico/exercicio02.js
+++ desenvolvimento dinamico/exercicio02.js
@@ -1,5 +1,11 @@
 let meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro",];
 
-let primeiroTrimestre = meses.slice(0,3)
+let primeiroTrimestre = meses.slice(0,3);
+console.log(primeiroTrimestre);
+let segundoTrimestre = meses.slice(3,6);
+console.log(segundoTrimestre);
+let terceiroTrimestre = meses.slice(6,9);
+console.log(terceiroTrimestre);
+let quartoTrimestre = meses.slice(9,12);
+console.log(quartoTrimestre);
 
-console.log(primeiroTrimestre)
