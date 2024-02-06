074f9133c575eaf700c881850167ac8083eae374
igorfariasoliveira <igor10farias@gmail.com>
2024-02-06 08:48:58 -0300

    Update switch.js

--- desenvolvimento dinamico/switch.js
+++ desenvolvimento dinamico/switch.js
@@ -1,2 +1,17 @@
+
 console.log("Qual o melhor jogo Ja feito?\n 1: Chrono Trigger\n 2: The last of Us\n 3: Kingdom Hearts");
-let melhor = prompt("Escolha o melhor jogo ja feito");
+let melhor = 0;
+
+switch(melhor){
+  case 1:
+    console.log("O melhor jogo é o Chrono Trigger");
+  break
+  case 2:
+    console.log("O melhor jogo é o The Last of Us");
+  break
+  case 3:
+    console.log("O melhor jogo é o Kingdom Hearts");
+  break
+  default:
+    console.log("Você tem que escolher")
+}
