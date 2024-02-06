79746198544062335a9a84a23f3f3710d6c49174
igorfariasoliveira <igor10farias@gmail.com>
2024-02-06 08:42:30 -0300

    treino

--- desenvolvimento dinamico/switch.js
+++ desenvolvimento dinamico/switch.js
@@ -1,2 +1,2 @@
-console.log()
-let melhor = prompt("Escolha o melhor jo")
+console.log("Qual o melhor jogo Ja feito?\n 1: Chrono Trigger\n 2: The last of Us\n 3: Kingdom Hearts");
+let melhor = prompt("Escolha o melhor jogo ja feito");
